e65d1e65f0a85923964e60db5f8a05a2d2abf83f
annnahitttzd <anahitkhachatryan05.zd@gmail.com>
2024-01-26 18:39:01 +0400

    final changes

--- app/Console/Commands/CalculateCommission.php
+++ app/Console/Commands/CalculateCommission.php
@@ -5,27 +5,42 @@ namespace App\Console\Commands;
 use App\Services\CommissionCalculatorService;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 
 class CalculateCommission extends Command
 {
     protected $signature = 'app:calculate-commission';
     protected $description = 'Command description';
-
     public function handle()
     {
         $csvPath = storage_path('app/public/operations.csv');
         $csvFile = fopen($csvPath, 'r');
         while (($row = fgetcsv($csvFile)) !== false) {
-            $operations[] = $row;
-            }
-        fclose($csvFile);
-//        $calculator = new CommissionCalculatorService();
-//        $calculator->calculateCommissionForUser($operations);
-        $calculator = app(CommissionCalculatorService::class);
-        foreach ($operations as $row) {
-            $commission = $calculator->calculateCommission($row);
-//            dump( $commission);
+            $operations[] = [
+                'date' => $row[0],
+                'user_id' => intval($row[1]),
+                'client_type' => $row[2],
+                'operation_type' => $row[3],
+                'amount' => floatval($row[4]),
+                'currency' => $row[5]
+            ];
         }
+        usort($operations, function ($a, $b) {
+           return Carbon::parse($a['date'])->gt(Carbon::parse($b['date']));
+        });
+        $operation_index = 0;
+        $request = Http::get('https://developers.paysera.com/tasks/api/currency-exchange-rates');
+        $response = $request->json();
+        $rates = $response['rates'];
+        foreach ($operations as &$operation) {
+            $operation['operation_index'] = $operation_index;
+            $operation_index++;
+            $operation['amount_eur'] = $operation['currency'] == "EUR" ? $operation['amount'] : $operation['amount'] / $rates[$operation['currency']];
+        }
+        $operations = collect($operations);
+        /** @var CommissionCalculatorService $calculator */
+        $calculator = app(CommissionCalculatorService::class);
+        $commissions = $calculator->calculateCommissions($operations, $rates);
+        $this->info(implode("\n", $commissions));
     }
 }
-// $users[$row->userId] = $row
--- app/Services/CommissionCalculatorService.php
+++ app/Services/CommissionCalculatorService.php
@@ -3,64 +3,53 @@
 namespace App\Services;
 
 use Carbon\Carbon;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 
-    class   CommissionCalculatorService
+    class  CommissionCalculatorService
     {
-        private $userWithdrawnAmountThisWeek =[];
-        private $withdrawalCountThisWeek = [];
-    public function calculateCommission(array $operations)
+    public function calculateCommissions(Collection $operations, $rates)
     {
-
-        $operationDate = Carbon::parse($operations[0]);
-        $userId = $operations[1];
-        $clientType = $operations[2];
-        $operationType = $operations[3];
-        $amount = $operations[4];
-        $currency = $operations[5];
-        if ($operationType === 'deposit') {
-            return $this->calculateDepositCommission($amount);
-        }elseif ($operationType === 'withdraw') {
-            return $this->calculateWithdrawCommission($userId, $clientType, $operationDate, $amount);
+        $commissions = [];
+        foreach ($operations as $operation) {
+            if ($operation['operation_type'] == 'withdraw') {
+                $commissions[] = $this->calculateWithdrawCommission($operation, $operations, $rates);
+            } elseif ($operation['operation_type'] == 'deposit') {
+                $commissions[] = $this->calculateDepositCommission($operation['amount_eur']);
+            }
         }
-        return 0;
+        return $commissions;
     }
+
+        private function calculateWithdrawCommission($operation, $operations, $rates)
+        {
+            $feePercentage = $operation['client_type'] == 'private' ? 0.003 : 0.005;
+            $freeChargeAmount = 0;
+
+            if ($operation['client_type'] == 'private') {
+                $operationsBeforeInCurrentWeek = $operations->where('date', '>=', Carbon::parse($operation['date'])->startOfWeek()->toDateString())
+                    ->where('user_id', $operation['user_id'])->where('operation_index', '<', $operation['operation_index'])
+                    ->where('operation_type', 'withdraw')
+                    ->where('client_type', 'private');
+                $amountSum = $operationsBeforeInCurrentWeek->sum('amount_eur');
+                if ($operationsBeforeInCurrentWeek->count() < 3 && $amountSum < 1000) {
+                    $freeChargeAmount = 1000 - $amountSum;
+                }
+            }
+
+            $freeChargeAmount = $operation['currency'] == 'EUR' ? $freeChargeAmount : $freeChargeAmount * $rates[$operation['currency']];
+
+            if ($operation['amount'] > $freeChargeAmount) {
+                $amountToCalculateFee = $operation['amount'] - $freeChargeAmount;
+            } else {
+                $amountToCalculateFee = 0;
+            }
+
+            return number_format(ceil($amountToCalculateFee * $feePercentage * 100) / 100, 2);
+        }
         private function calculateDepositCommission($amount)
         {
-            return $amount * 0.0003;
-        }
-        private function calculateWithdrawCommission($userId, $clientType,$operationDate, $amount)
-        {
-            if (!isset($this->userWithdrawnAmountThisWeek[$userId])) {
-                $this->userWithdrawnAmountThisWeek[$userId] = 0;
-                $this->withdrawalCountThisWeek[$userId] = 0;
-            }
-            if ($clientType === 'private') {
-                if ($this->freeOfCharge($userId, $operationDate, $amount)) {
-                    return 0;
-                }
-                return $amount * 0.003;
-            } elseif ($clientType === 'business'){
-                return $amount * 0.005;
-            }
-            return 0;
-        }
-        private function freeOfCharge($userId, $operationDate, $amount)
-        {
-            $startOfWeek = Carbon::parse($operationDate)->startOfWeek();
-            $endOfWeek = Carbon::parse($operationDate)->endOfWeek();
-            if ($operationDate->isBetween($startOfWeek, $endOfWeek)) {
-            $this->withdrawalCountThisWeek[$userId]++;
-            if ($this->withdrawalCountThisWeek[$userId] <= 3) {
-             $this->userWithdrawnAmountThisWeek[$userId] += $amount;
-
-                if ($this->userWithdrawnAmountThisWeek[$userId] <= 1000.00) {
-                return true;
-                } else {
-                 return $amount - (1000.00 - $this->userWithdrawnAmountThisWeek[$userId]);
-                }
-            }
-        }
-            return false;
+            return number_format(ceil($amount * 0.0003 * 100) / 100, 2);
         }
     }
